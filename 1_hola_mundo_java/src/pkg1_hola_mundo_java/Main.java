da64e3b13afe1228925b8939971a5945346a2105
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-11-15 18:23:03 -0600

    Add files via upload

---
+++ 1_hola_mundo_java/src/pkg1_hola_mundo_java/Main.java
@@ -0,0 +1,20 @@
+/*Este es mi primer programa en java, muestra como imprimir en pantalla 
+un mensaje, usando el objeto System.out y su método llamado println.
+ */
+package pkg1_hola_mundo_java;
+
+/**
+ *
+ * @author Emilio Carcaño Bringas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Estoy orgulloso de pertenecer a Uninaciones y estudiar en la UNAM");
+        }
+    
+}
